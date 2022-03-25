c7b6f67bd99e24a27cf5f1bf030a6393120c39b7
evvfebruary <Firebulletred14Yn>
2022-03-25 23:31:21 +0300

    # Try entrypoint instead cmd

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup python3 -m uvicorn --app-dir ./code main:app --host 0.0.0.0 --port 8080 > /code/output.log
+nohup python3 -m uvicorn --app-dir ./code main:app --host 0.0.0.0 --port 8080 > /code/output.log & ovpn_run
