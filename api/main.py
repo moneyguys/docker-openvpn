d17645444d98e89a7b26b9e1e5e7594e46b322c7
evvfebruary <Firebulletred14Yn>
2022-03-25 22:39:52 +0300

    # Add generationd and fix requirements

--- api/main.py
+++ api/main.py
@@ -1,10 +1,40 @@
-from typing import Optional
-
+import os
+import subprocess
+from loguru import logger
+from pydantic import BaseModel
 from fastapi import FastAPI
+
+
+class CertificateCreateRequest(BaseModel):
+    certificate_user: str
+    expired_in: int = None
+
+
+def create_certificate_for_user(certificate_user: str,
+                                expired_in: int = None):
+    cert_path, err = None, None
+    add_user_cmd = f'easyrsa build-client-full {certificate_user} nopass'
+    user_cert_folder = '/etc/openvpn/user-certs/'
+    cert_filename = f'{certificate_user}.ovpn'
+    user_cert_abs_path = os.path.join(user_cert_folder, cert_filename)
+    generate_cert = f'ovpn_getclient {certificate_user} >  {user_cert_abs_path}'
+    final_cmd = ";".join([add_user_cmd, generate_cert])
+    pipe = subprocess.Popen(final_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res = pipe.communicate()
+    retcode = pipe.returncode
+    logger.debug(f"res={res}, retcode={retcode}")
+    if retcode != 0:
+        error_text = res[-1]
+        return cert_path, error_text
+    return user_cert_abs_path, err
+
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/create_user_certificate")
+def read_root(certificate_create_request: CertificateCreateRequest):
+    certificate_user = certificate_create_request.certificate_user
+    expired_in = certificate_create_request.expired_in
+    cert_path, err = create_certificate_for_user(certificate_user,expired_in)
+    return {"certificate_user": certificate_user, "expired_in": expired_in, 'cert_path': cert_path, 'err':err}
